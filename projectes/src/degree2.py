42e6446e5b914a6321ed7249c1014a610140dca5
admin <admin@LENOVO>
2023-02-19 16:48:36 +0530

    Second commit

---
+++ projectes/src/degree2.py
@@ -0,0 +1,16 @@
+class degree:
+    def getdegree(self): 
+        print("i have got degree")
+class undergraduate:
+    def getdegree(self):
+        print("i am undergraduate")
+class postgraduate:
+    def getdegree(self):
+        print("i am postgraduate")
+a=postgraduate()
+a.getdegree()
+a1=undergraduate()
+a1.getdegree()
+a2=degree()
+a2.getdegree()
+       
